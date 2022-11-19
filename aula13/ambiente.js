161d5a99e20da51d733452b101a84afefd35298b
Iago Valverde <iagopachiani96@gmail.com>
2022-11-19 15:03:18 -0300

    create aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+// Teste Lógico no Final
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 6)
+
+
+/*
+Teste Lógico no Início
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++ // c = c + 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
